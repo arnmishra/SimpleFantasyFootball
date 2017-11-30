1fe9038de0b6229f4229a23297306b4236bfd54f
Arnav Mishra <arnmishra@gmail.com>
2017-11-30 16:24:24 -0600

    allows loading leagues with get request

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 import datetime
 from project import app, db
-from models import Team, Game
+from models import Team, League
 from flask import render_template, url_for, request, redirect
 from project.scripts.football_api import make_teams, trade_in_players, get_games, get_player_scores
 
@@ -31,52 +31,61 @@ def set_up_teams():
     num_teams = int(request.form["num_teams"])
     return render_template("set_up_teams.html", num_teams=num_teams)
 
-@app.route("/view_all_teams", methods=['GET', 'POST'])
-def view_all_teams():
+@app.route('/view_all_teams', defaults={'league_name': None}, methods=['GET', 'POST'])
+@app.route("/view_all_teams/<league_name>", methods=['GET', 'POST'])
+def view_all_teams(league_name):
     """ Renders a view of the players on every team
 
     :return: view_all_teams
     """
     if request.method == "GET":
         redirect("/")
-    team_names = request.form.getlist("team_names")
-    game_name = request.form["game_name"]
+    if "team_names" in request.form:
+        team_names = request.form.getlist("team_names")
+    else:
+        team_names = []
+    if "league_name" in request.form:
+        league_name = request.form["league_name"]
     teams = []
-    game = Game.query.filter_by(game_name=game_name).first()
-    if not game:
-        game = Game(game_name)
-        db.session.add(game)
+    league = League.query.filter_by(league_name=league_name).first()
+    new_league = False
+    if not league:
+        league = League(league_name)
+        db.session.add(league)
+        new_league = True
     for team_name in team_names:
         team = Team.query.filter_by(team_name=team_name).first()
         if not team:
-            team = Team(game_name, team_name, "None")
+            team = Team(league_name, team_name, "None")
             db.session.add(team)
         teams.append(team)
-    make_teams(teams, game)
+    if new_league:
+        make_teams(teams, league)
+    else:
+        teams = Team.query.filter_by(league_name=league_name)
     db.session.commit()
 
     """
         Return all this week's games
     """
     games = get_games(13)
-    print games
-    return render_template("view_all_teams.html", game_name=game_name, teams=teams, games=games)
+    return render_template("view_all_teams.html", league_name=league_name, teams=teams, games=games)
 
-@app.route("/team/<game_name>/<team_name>", methods=['GET', 'POST'])
-def view_team(game_name, team_name):
+@app.route("/team/<league_name>/<team_name>", methods=['GET', 'POST'])
+def view_team(league_name, team_name):
     """ Renders a page with only your team and their scores
 
     :return: view_team.html
     """
-    team = Team.query.filter_by(game_name=game_name, team_name=team_name).first()
-    game = Game.query.filter_by(game_name=game_name).first()
+    team = Team.query.filter_by(league_name=league_name, team_name=team_name).first()
+    league = League.query.filter_by(league_name=league_name).first()
     if request.method == 'POST':
         if "star" in request.form:
             team.starred_position = request.form["star"]
         if "trade_players" in request.form:
             trade_players = request.form.getlist("trade_players")
-            trade_in_players(team, game, trade_players)
+            trade_in_players(team, league, trade_players)
     if request.method == 'GET':
         get_player_scores(team)
     db.session.commit()
-    return render_template("view_team.html", team=team, game_name=game_name)
+    return render_template("view_team.html", team=team, league_name=league_name)
